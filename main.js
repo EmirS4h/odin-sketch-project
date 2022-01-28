523456562a19c1804d349361306d8f2a100901f1
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-01-28 11:04:47 +0300

    Added Rgb and Hsl Color Mods

--- main.js
+++ main.js
@@ -2,15 +2,37 @@ const container = document.querySelector("#grid-container");
 const gridSizeText = document.querySelector("#gridSize");
 
 const toggleGridBtn = document.querySelector("#toggleBtn");
+const rgbaBtn = document.querySelector("#rgbaBtn");
+rgbaBtn.addEventListener("click",() => {
+  colorMode = 1;
+})
+const hslBtn = document.querySelector("#hslBtn");
+hslBtn.addEventListener("click",() => {
+  colorMode = 2;
+})
 
+let colorMode = 0;
 let currentGridSize = 16;
 let currentPaint = "black";
 
 let grid_items = container.children;
 
+const createRandomColorRGBA = () => {
+  color = Math.floor(Math.random() * 256);
+  return color;
+};
+
+const createRandomColorHSL = () => {
+  h = Math.floor(Math.random() * 360);
+  s = Math.floor(Math.random() * 100) + "%";
+  l = Math.floor(Math.random() * 100) + "%";
+  colors = [h, s, l];
+  return colors;
+};
+
 const colorPicker = document.querySelector("#colorPicker");
 // Change Color
-colorPicker.onchange = (e) => (currentPaint = e.target.value);
+colorPicker.onchange = (e) => (colorMode = 0,currentPaint = e.target.value);
 // Change Grid Size
 const sizeSlider = document.querySelector("#sizeSlider");
 sizeSlider.onchange = (e) => {
@@ -24,8 +46,15 @@ sizeSlider.onmousemove = (e) => {
 };
 
 // Paint Grid
-const paintGrid = (grid_item, paint) => {
-  grid_item.target.style.backgroundColor = paint;
+const paintGrid = (grid_item, paint, colorMode) => {
+  if (colorMode === 1) {
+    grid_item.target.style.backgroundColor = `rgba(${createRandomColorRGBA()},${createRandomColorRGBA()},${createRandomColorRGBA()},1)`;
+  } else if (colorMode === 2) {
+    hslColor = createRandomColorHSL();
+    grid_item.target.style.backgroundColor = `hsl(${hslColor[0]},${hslColor[1]},${hslColor[2]})`;
+  } else {
+    grid_item.target.style.backgroundColor = paint;
+  }
 };
 
 const create_grid_items = (number_of_grid_items, grid_container) => {
@@ -33,7 +62,7 @@ const create_grid_items = (number_of_grid_items, grid_container) => {
     const grid_item = document.createElement("div");
     grid_item.className = "grid-item-border";
     grid_item.addEventListener("mouseover", (e) => {
-      paintGrid(e, currentPaint);
+      paintGrid(e, currentPaint, colorMode);
     });
     grid_container.appendChild(grid_item);
   }
